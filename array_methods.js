048b6f7d1a4c84b8e035f07825b857b40ab9ac8c
abhishekkhot <abhishek.khot@idfy.com>
2024-11-16 16:09:42 +0530

    addition

--- array_methods.js
+++ array_methods.js
@@ -1,132 +1,263 @@
 let names = ["Danny", "Joe", "Sarah", "Molly"];
 
-//1.toString
-console.log("toString: ", names.toString());
+// //1.toString
+// console.log("toString: ", names.toString());
 
-//2.join
-console.log("join: ", names.join(","));
+// //2.join
+// console.log("join: ", names.join(","));
 
-//3.concate
-console.log(
-  "concate: ",
-  names.concat(["Dan", "Jolly", "Rock"], ["Mat", "Leo"])
-);
-console.log("after concate: ", names); //original array doesn't changed
+// //3.concate
+// console.log(
+//   "concate: ",
+//   names.concat(["Dan", "Jolly", "Rock"], ["Mat", "Leo"])
+// );
+// console.log("after concate: ", names); //original array doesn't changed
 
-//4.splice (remove or add elements from array)
-let res = names.splice(1, 1);
-console.log("removed with splice: ", res);
-console.log("remaining after splice: ", names);
+// //4.splice (remove or add elements from array) mutats original array
+// let res = names.splice(1, 1);
+// names.splice(start)
+// names.splice(start,deleteCount)
+// names.splice(start,deleteCount,item1) //replaces item1 from start upto delete count
+// names.splice(start,deleteCount,item1,item2,item3,...itemN) //all items would be added after removing deleteCount elements from start index of array
+// console.log("removed with splice: ", res);
+// console.log("remaining after splice: ", names);
 
-//add values using splice
-names.splice(3, 2, "Rakesh", "Ramesh", "Ramesh");
-console.log(names);
+// //add values using splice
+// names.splice(3, 2, "Rakesh", "Ramesh", "Ramesh");
+// console.log(names);
 
-//5.slice (slice out part of an array)
-console.log("slice: ", names.slice(1, 3)); //from index, upto index(exclusive index)
-console.log("after slice original array: ", names); //original kept as it is, slice works on copy of array
+// //5.slice (slice out part of an array)
+// console.log("slice: ", names.slice(1, 3)); //from index, upto index(exclusive index)
+// console.log("after slice original array: ", names); //original kept as it is, slice works on copy of array
 
-//6.indexOf (find first occurrence element inside array)
-let firstIndex = names.indexOf("Ramesh");
-console.log("Ramesh's 1st index: ", firstIndex);
+// //6.indexOf (find first occurrence element inside array)
+// let firstIndex = names.indexOf("Ramesh");
+// console.log("Ramesh's 1st index: ", firstIndex);
 
-//7.lastIndexOf (find last occurrence of element inside array)
-let lastIndex = names.lastIndexOf("Ramesh");
-console.log("Ramesh's last index: ", lastIndex);
+// //7.lastIndexOf (find last occurrence of element inside array)
+// let lastIndex = names.lastIndexOf("Ramesh");
+// console.log("Ramesh's last index: ", lastIndex);
 
-// 8.flat
-let nums = [1, 2, 3, 4, [5, 6], 7];
-console.log("before flat: ", nums);
-console.log("flatten array: ", nums.flat());
-console.log("after flat: ", nums);
-// before and after will be same as flat works on copy of array
+// // 8.flat
+// let nums = [1, 2, 3, 4, [5, 6], 7];
+// console.log("before flat: ", nums);
+// console.log("flatten array: ", nums.flat());
+// console.log("after flat: ", nums);
+// // before and after will be same as flat works on copy of array
 
-// flat function takes number of levels deep flaten the array, default is 1
-nums.push([8, [9, 10]]);
-console.log("nested array: ", nums.flat(2)); //this will flaten 2nd level nesting
+// // flat function takes number of levels deep flaten the array, default is 1
+// nums.push([8, [9, 10]]);
+// console.log("nested array: ", nums.flat(2)); //this will flaten 2nd level nesting
 
-// Higher order array functions
-// 9.forEach
-nums.forEach((no) => {
-  console.log(no);
-});
-//more controlled apporach
-for (let i = 0; i < nums.length; i++) {
-  console.log(nums[i]);
-}
+// // Higher order array functions
+// // 9.forEach
+// nums.forEach((no) => {
+//   console.log(no);
+// });
+// //more controlled apporach
+// for (let i = 0; i < nums.length; i++) {
+//   console.log(nums[i]);
+// }
 
-// 10.map
-let nums = [1, 2, 3, 4, 5];
-console.log("before map: ", nums);
-let doubledArray = nums.map((no) => no * 2);
-console.log("doubled array; ", doubledArray);
-console.log("after map: ", nums);
+// // 10.map
+// let nums = [1, 2, 3, 4, 5];
+// console.log("before map: ", nums);
+// let doubledArray = nums.map((no) => no * 2);
+// console.log("doubled array; ", doubledArray);
+// console.log("after map: ", nums);
 
-// 11.filter
-let posts = [
-  { title: "post 1", author: "Dan" },
-  { title: "post 2", author: "Sarah" },
-  { title: "post 3", author: "Dan" },
-  { title: "post 4", author: "Dan" },
-];
-console.log("before filter: ", posts);
-let dansPosts = posts.filter((post) => post.author === "Dan");
-console.log("dan's posts: ", dansPosts);
-console.log("after filter: ", posts);
+// // 11.filter
+// let posts = [
+//   { title: "post 1", author: "Dan" },
+//   { title: "post 2", author: "Sarah" },
+//   { title: "post 3", author: "Dan" },
+//   { title: "post 4", author: "Dan" },
+// ];
+// console.log("before filter: ", posts);
+// let dansPosts = posts.filter((post) => post.author === "Dan");
+// console.log("dan's posts: ", dansPosts);
+// console.log("after filter: ", posts);
 
-// 12.reduce (takes an array and reduce it into single value)
-let nums = [1, 2, 3, 4, 5, 6];
-console.log("before reduce: ", nums);
-let sum = nums.reduce((sum, currNo) => sum + currNo);
-console.log("sum using reduce: ", sum);
-console.log("after reduce: ", nums);
+// // 12.reduce (takes an array and reduce it into single value)
+// let nums = [1, 2, 3, 4, 5, 6];
+// console.log("before reduce: ", nums);
+// let sum = nums.reduce((sum, currNo) => sum + currNo);
+// console.log("sum using reduce: ", sum);
+// console.log("after reduce: ", nums);
 
-let sum2 = nums.reduce((sum, currNo) => sum + currNo, 10); //we can pass default sum value i.e 10
-console.log(sum2);
+// let sum2 = nums.reduce((sum, currNo) => sum + currNo, 10); //we can pass default sum value i.e 10
+// console.log(sum2);
 
-//find max no inside array using reduce
-let maxNo = nums.reduce((a, b) => (a > b ? a : b));
-console.log("maximum no with reduce: ", maxNo);
+// //find max no inside array using reduce
+// let maxNo = nums.reduce((a, b) => (a > b ? a : b));
+// console.log("maximum no with reduce: ", maxNo);
 
-// 13.some() some of elements passes the given condition the returns true
-let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let isGreaterThan5 = nums.some((no) => no > 5);
-console.log(isGreaterThan5);
+// // 13.some() some of elements passes the given condition the returns true
+// let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let isGreaterThan5 = nums.some((no) => no > 5);
+// console.log(isGreaterThan5);
 
-// 14.every() every elements of the array passes the given condition
-let nums = [1, 2, 3, 4, 5];
-let isAllGreaterThan0 = nums.every((no) => no > 0);
-console.log(isAllGreaterThan0);
+// // 14.every() every elements of the array passes the given condition
+// let nums = [1, 2, 3, 4, 5];
+// let isAllGreaterThan0 = nums.every((no) => no > 0);
+// console.log(isAllGreaterThan0);
 
-// 15.find
-let stock = [
-  { item: "foot balls", quantity: 100 },
-  { item: "basket balls", quantity: 200 },
-  { item: "tennis balls", quantity: 500 },
-];
+// // 15.find
+// let stock = [
+//   { item: "foot balls", quantity: 100 },
+//   { item: "basket balls", quantity: 200 },
+//   { item: "tennis balls", quantity: 500 },
+// ];
 
-let footsBalls = stock.find((stock) => stock.item === "foot balls");
-console.log(footsBalls);
+// let footsBalls = stock.find((stock) => stock.item === "foot balls");
+// console.log(footsBalls);
 
-// 16.findIndex returns index of first occurrence of element passing the condition
-console.log(stock.findIndex((stock) => stock.item === "tennis balls"));
+// // 16.findIndex returns index of first occurrence of element passing the condition
+// console.log(stock.findIndex((stock) => stock.item === "tennis balls"));
 
-//17.sort default sort works on javascript strings
-let names = ["Ramesh", "Abhishek", "Rakesh", "Zeshaan", "Priya"];
-console.log("before sort: ", names);
-names.sort();
-console.log("after sort: ", names);
+// //17.sort default sort works on javascript strings (mutates original array)
+// sort by default converts elements inside array into string and compare them unto utf16 encoding
+// let names = ["Ramesh", "Abhishek", "Rakesh", "Zeshaan", "Priya"];
+// console.log("before sort: ", names);
+// names.sort();
+// console.log("after sort: ", names);
 
-//sorting numbers
-let numbers = [4, 5, 1, 3, 2];
-numbers.sort();
-console.log(numbers); //this will sort the numbers this is done on the basis of 1'st digit only
+// //sorting numbers
+// let numbers = [4, 5, 1, 3, 2];
+// numbers.sort();
+// console.log(numbers); //this will sort the numbers this is done on the basis of 1'st digit only
 
-let nums = [10, 12, 22, 33, 20, 4, 5];
-nums.sort();
-console.log("incorrect sorting: ", nums); //this will not get sorted as these 2 digit numbers
-//actual solution is to pass callback function
-nums.sort((a, b) => a - b);
-//to sort in decreasig order
-// nums.sort((a, b) => b - a);
-console.log("correct sorting: ", nums);
+// let nums = [10, 12, 22, 33, 20, 4, 5];
+// nums.sort();
+// console.log("incorrect sorting: ", nums); //this will not get sorted as these 2 digit numbers
+// //actual solution is to pass callback function
+// nums.sort((a, b) => a - b);
+// //to sort in decreasig order
+// // nums.sort((a, b) => b - a);
+// console.log("correct sorting: ", nums);
+
+//18.includes return true if element is present inside array
+// let array = ["a", "b", "c", "d"];
+// console.log(array.includes("a"));
+// console.log(array.includes("a", 1));
+
+//19.push adds element at the end of the array and return new length of the array
+// let len = array.push("e");
+// console.log(len);
+// console.log(array);
+
+//20.unshift adds element at start of the array and returns new length of the array
+// let newLen = array.unshift("z");
+// console.log(newLen);
+// console.log(array);
+
+//21.pop removes last element from array and returns it
+// let lastChar = array.pop();
+// console.log(lastChar);
+// console.log(array);
+
+//22.shift removes first element from array
+// let firstChar = array.shift();
+// console.log(firstChar);
+// console.log(array);
+
+//23.fill fills element inside array and returns modified array (takes start index and end index between which elements are filled)
+// let fillRes = array.fill("x", 1); //mutates original array
+// console.log(fillRes);
+// console.log(array);
+// array[3] = "w";
+// console.log(array);
+
+//24.copy copies elements within array and returns the new mutated array
+// let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// array.copyWithin(targetIndex);
+// array.copyWithin(targetIndex, startIndex);
+// array.copyWithin(targetIndex, startIndex, endIndex);
+// let copyRes = nums.copyWithin(1);
+// console.log(copyRes);
+// console.log(nums);
+// nums.copyWithin(0, 2, 5);
+// console.log(nums);
+
+//25.reverse reverse order of elements in array
+// Reverses the elements in an array in place.
+// This method mutates the array and returns a reference to the same array.
+// let nums = [1, 2, 3, 4, 5];
+// let reversedNums = nums.reverse();
+// console.log(reversedNums);
+// console.log(nums);
+
+//26.from Creates an array from an iterable object.
+// Array.from(arrayLikeObject iterable object)
+// let str = "12345";
+// const arrayOfStringNumbers = Array.from(str);
+// console.log(arrayOfStringNumbers);
+// console.log(str);
+// const arrayOfNumbers = Array.from(str, (strNo) => Number(strNo));
+// console.log(arrayOfNumbers);
+// console.log(str);
+
+//27.isArray returns if passed object/value is instance of array
+// const obj = { key: "value" };
+// console.log(Array.isArray(obj));
+// console.log(obj);
+
+//28.valueOf Returns the primitive value of the specified object. does not mutate original array
+// const items = ["Laptop", "Phone", "iPad", "Charger"];
+// const itemsCopy = items.valueOf();
+// console.log(itemsCopy);
+// console.log(items);
+
+//29.entires Returns an iterable of key, value pairs for every entry in the array
+// const nums = [1, 2, 3, 4, 5];
+// const numsEntry = nums.entries();
+// console.log(nums);
+// console.log(...numsEntry);
+// for (let entry of numsEntry) {
+//   console.log(entry[1]);
+// }
+
+//30.keys Returns an iterable of keys in the array does not mutates original array
+// const chars = ["a", "b", "c", "d", "e"];
+// const charKeys = chars.keys();
+// console.log(chars);
+// console.log(...charKeys);
+// for (let key of charKeys) {
+//   console.log(key, chars[key]);
+// }
+
+//31.values Returns an iterable of values in the array does not mutates original array
+// const items = ["a", "b", "c", "d", "e"];
+// const itemValues = items.values();
+// console.log(items);
+// console.log(...itemValues);
+
+//32.reduce does not mutates original array
+// let nums = [1, 2, 3, 4, 5];
+// function reducer(prevVal, currVal) {
+//   return prevVal + currVal;
+// }
+// let sum = nums.reduce(reducer);
+// console.log(sum);
+// console.log(nums);
+
+//33.reduce right same as reduce only it start from right side of array
+// let nums = [1, 2, 3, 4];
+// let sum = nums.reduceRight((prevValue, currValue) => prevValue + currValue);
+// console.log(sum);
+
+//34.flat flattens the nested array does not mutates the original array returns new flateen array
+// let nums = [1, 2, 3, [4, [5, [6]]]];
+// console.log(nums.flat()); //[1,2,3,4,[5,[6]]]
+// console.log(nums.flat(2)); //[1,2,3,4,5,[6]]
+// console.log(nums.flat(3)); //[1,2,3,4,5,6]
+// console.log(nums.flat(4)); //[1,2,3,4,5,6]
+
+//35.flatMap map+flat function it's like calling map and then flat with depth of 1 nested array
+// let nums = [1, 2, 3, [4, 5, 6]];
+// console.log(
+//   nums.flatMap((arr) => {
+//     console.log(arr);
+//     // return arr[0] + arr[1];
+//   })
+// );
--- questions.js
+++ questions.js
@@ -79,4 +79,331 @@ console.log(Object.getPrototypeOf(alice.sayName()));
 console.log(alice.__proto__.sayName());
 */
 
+//1.mapping users to user names
+// const users = [
+//   { id: 1, name: "Alice", isActive: true },
+//   { id: 2, name: "Bob", isActive: false },
+//   { id: 3, name: "Mario", isActive: true },
+// ];
+// const userNames = users.map((user) => user.name);
+// console.log(userNames);
 
+// //2.differnce between null and undefined
+// let var1;
+// console.log(var1);
+// console.log(typeof var1);
+
+// let var2 = null;
+// console.log(var2);
+// console.log(typeof var2);
+
+//3.hosting in js
+// console.log(foo);
+// let foo = 10;
+// var foo;
+// foo = 10;
+// console.log(foo);
+
+//clousers
+//4.create a counter function
+// const counter = (num) => {
+//   return {
+//     increment: function () {
+//       num++;
+//       console.log(num);
+//     },
+//     decrement: function () {
+//       num--;
+//       console.log(num);
+//     },
+//   };
+// };
+// let count = counter(10);
+// count.increment();
+// count.decrement();
+
+//5.create a function which stores a string and returns
+// function getUser() {
+//   let user = {
+//     name: "Alice",
+//     age: 21,
+//     isActive: false,
+//   };
+//   return {
+//     getName: function () {
+//       return user.name;
+//     },
+//   };
+// }
+
+// const alice = getUser();
+// console.log(alice.getName());
+
+//currying
+//6.write a function which helps to multiply (a)(b) and returns multiplication of a and b
+// function getMultiplication(a) {
+//   return function (b) {
+//     return a * b;
+//   };
+// }
+
+// console.log(getMultiplication(10)(20));
+
+//7.create partially sum function
+// function getSum(a) {
+//   return function (b) {
+//     return function (c) {
+//       return a + b + c;
+//     };
+//   };
+// }
+
+// const sumA = getSum(10);
+// console.log(sumA);
+// const sumB = sumA(20);
+// console.log(sumB);
+// const sumC = sumB(30);
+// console.log(sumC);
+
+//8.
+// function appendNumber(nums, no) {
+//   nums.push(no);
+//   return nums;
+// }
+// let nums = [1, 2];
+// console.log(appendNumber(nums, 3));
+
+//9.
+// const users = [
+//   { id: 1, name: "Alice", isActive: false },
+//   { id: 2, name: "Bob", isActive: true },
+//   { id: 3, name: "Jack", isActive: true },
+//   { id: 4, name: "Ramesh", isActive: true },
+//   { id: 5, name: "Suresh", isActive: false },
+// ];
+
+// function checkIsUserPresentAndActive(userName) {
+//   return users.some((user) => user.name === userName && user.isActive);
+// }
+
+// console.log(checkIsUserPresentAndActive("Alice"));
+// console.log(checkIsUserPresentAndActive("Ramesh"));
+
+//10.remove duplicate from array
+// function removeDuplicateUsers(users) {
+//   const uniqueUsers = new Set();
+//   users.forEach((user) => uniqueUsers.add(user));
+//   return uniqueUsers;
+// }
+// const users = [
+//   "Alice",
+//   "Alice",
+//   "Jack",
+//   "Ramesh",
+//   "Ram",
+//   "Bob",
+//   "Suresh",
+//   "Rakesh",
+//   "Suresh",
+//   "Bob",
+// ];
+// console.log(removeDuplicateUsers(users));
+
+//11.sort array of objects by author name
+// const bookAuthors = [
+//   { bookName: "Harry Potter", author: "Abby" },
+//   { bookName: "Warcorss", author: "Maria Lu" },
+//   { bookName: "The hunger games", author: "Bob Marley" },
+// ];
+// bookAuthors.sort((a, b) => {
+//   return a.author.split(" ")[0].localeCompare(b.author.split(" ")[0]);
+// });
+// console.log(bookAuthors);
+
+//12.write a range function
+// function range(start, end) {
+//   if (start == end) {
+//     return [start];
+//   }
+//   return [start, ...range(start + 1, end)];
+// }
+// console.log(range(1, 10));
+
+//13. number of occurences of minimum number in an array
+// const nums = [1, 2, 3, 4, 5, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+// const min = Math.min(...nums);
+// const minNumbersArray = nums.filter((num) => num == min);
+// console.log(nums);
+// console.log(minNumbersArray.length);
+
+//14.global context
+// function getItem() {
+//   console.log(this);
+// }
+// getItem();
+
+//15. object context
+// const Item = {
+//   name: "Alice",
+//   getName() {
+//     console.log(this);
+//   },
+// };
+// Item.getName();
+
+//16.global context
+// const item = {
+//   name: "Bob",
+//   getName() {
+//     [1, 2, 3].map(function () {
+//       console.log(this);
+//     });
+//   },
+// };
+// item.getName();
+
+//17.
+// class Employee {
+//   constructor(id, name, department, designation) {
+//     this.id = id;
+//     this.name = name;
+//     this.salary = null;
+//     this.department = department;
+//     this.designation = designation;
+//   }
+//   setSalary(salary) {
+//     this.salary = salary;
+//   }
+//   getSalary() {
+//     return this.salary;
+//   }
+// }
+// const alice = new Employee(1, "Alice", "Marketing", "Manager");
+// console.log(alice.getSalary());
+// alice.setSalary(1000);
+// console.log(alice.getSalary());
+
+//18.same class with prototypes
+// function Employee(id, name, department, designation) {
+//   this.id = id;
+//   this.name = name;
+//   this.department = department;
+//   this.designation = designation;
+// }
+
+// Employee.prototype.setSalary = function (salary) {
+//   this.salary = salary;
+// };
+// Employee.prototype.getSalary = function () {
+//   return this.salary;
+// };
+
+// const alice = new Employee(1, "Alice", "Marketing", "Manager");
+// console.log(alice);
+// console.log(alice.getSalary());
+// alice.setSalary(1000);
+// console.log(alice.getSalary());
+
+//19. debounce function
+// function debounce(callback, timeoute) {
+//   let timer;
+//   return function (...args) {
+//     clearTimeout(timer);
+//     timer = setTimeout(() => {
+//       callback.apply(this, args);
+//     }, timeoute);
+//   };
+// }
+// function callbackFunction(name) {
+//   console.log(`Hello ${name}`);
+// }
+// const debounceFunction = debounce(callbackFunction, 500);
+// debounceFunction("Alice");
+// debounceFunction("Bob");
+// debounceFunction("Jack");
+
+//20.throttling
+// function throttle(callback, timeout) {
+//   let timer = null;
+//   return function (...args) {
+//     if (timer) return;
+//     timer = setTimeout(() => {
+//       timer = null;
+//       callback.apply(this, args);
+//     }, timeout);
+//   };
+// }
+
+// function callbackFunction(name) {
+//   console.log(`Hello from ${name}`);
+// }
+
+// const throttleFunction = throttle(callbackFunction, 500);
+// throttleFunction("Alice");
+// throttleFunction("Bob");
+// throttleFunction("Jack");
+
+//21. write an example of xmlhttprequest
+// const xhr = new XMLHttpRequest();
+
+// xhr.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
+
+// xhr.onload = function () {
+//   if (xhr.status >= 200 && xhr.status < 300) {
+//     const response = JSON.parse(xhr.responseText);
+//     console.log(response);
+//   } else {
+//     console.log("Request failed with status: ", xhr.status);
+//   }
+// };
+
+// xhr.onerror = function () {
+//   console.log("Network error occured");
+// };
+
+// xhr.send();
+
+// const url = "https://jsonplaceholder.typicode.com/posts";
+
+// fetch(url)
+//   .then((response) => response.json())
+//   .then((posts) => console.log(posts));
+
+// async function getPosts() {
+//   const response = await fetch(url);
+//   const posts = await response.json();
+//   console.log(posts);
+// }
+
+// getPosts();
+
+const asyncFun1 = (callback) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Function 1");
+      resolve("ONE");
+    }, 500);
+  });
+};
+const asyncFun2 = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Function 2");
+      resolve("TWO");
+    }, 200);
+  });
+};
+const asyncFun3 = (callback) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(first)
+    }, 100);
+  });
+};
+
+async function resolvePromises() {
+  const results = await Promise.all([asyncFun1, asyncFun2, asyncFun3]);
+  console.log(results);
+}
+
+resolvePromises();
